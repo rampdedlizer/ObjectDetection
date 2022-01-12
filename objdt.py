6096964a31b020b3a91f07aba8397225983a6b80
Pragyan <khatipragyan4@gmail.com>
2022-01-12 07:08:37 +0545

    Add boundary box

--- objdt.py
+++ objdt.py
@@ -1,6 +1,5 @@
 import cv2 
-
-img = cv2.imread('leo.jpg')
+img = cv2.imread('patty.jpg')
 
 #Empty matrix
 classNames= []
@@ -12,18 +11,23 @@ classfile = 'coco.names'
 with open(classfile,'rt') as f:
     classNames=f.read().rstrip('\n').split('\n')
 
-configpaths='ssd_mobilenet_v1_large_coco_2020_01_14.pbtxt'
-weightspath='frozen_interface_graph.pb'
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
 
 #Boundary box allocation 
-net=cv2.dnn_DetectionModel(weightspath,configpaths)
+net=cv2.dnn_DetectionModel(weightsPath,configPath)
 
 net.setInputSize(320,320)
 net.setInputScale(1.0/127.5)
-net.setInputMean(127.5,127.5,127.5)
-net.setInputSwapRB(True)
+net.setInputMean((127.5,127.5,127.5))
+net.setInputSwapRB(True)    
 
+classIds, confs, bbox = net.detect(img,confThreshold=0.5)
+print(classIds, confs)
 
+#Bounding box
+for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
 
 
 cv2.imshow("Output",img)
